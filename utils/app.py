3078f5e225a411733c7ca2bc1fb280731f2792d1
Sabith <masterdevsabith@gmail.com>
2025-07-12 13:27:20 +0530

    some file changes

---
+++ utils/app.py
@@ -0,0 +1,4 @@
+from find_max import find_max
+
+no = find_max([500, 6, 7, 200])
+print(no)
---
+++ utils/find_max.py
@@ -0,0 +1,9 @@
+
+
+def find_max(list):
+    max = list[0]
+    for n in list:
+        if n > max:
+            max = n
+
+    return max
