95a4b17d0b5c1a4851ddde846dc7770210be2600
Sabith <masterdevsabith@gmail.com>
2025-07-12 13:13:04 +0530

    class

---
+++ person_class.py
@@ -0,0 +1,11 @@
+class Person:
+    def __init__(self, name):
+        self.name = name
+
+    def talk(self):
+        print("HI I'm talking")
+
+
+person1 = Person("sabiiii")
+print(person1.name)
+person1.talk()
